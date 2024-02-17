355f45564b92aaee206cc3480bd7f146c5b6ac40
ian-afk <crkdosal@gmail.com>
2024-02-17 22:48:32 +0800

    initial state of frontend react

---
+++ frontend/src/pages/Todo.jsx
@@ -0,0 +1,13 @@
+export default function Todo() {
+  return (
+    <>
+      <form>
+        <input type="text" />
+        <button>Add</button>
+      </form>
+      <ul>
+        <li>Test</li>
+      </ul>
+    </>
+  );
+}
