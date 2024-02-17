be530d6a478b35209b35b530ead3466d32a431f5
ian-afk <crkdosal@gmail.com>
2024-02-17 23:15:36 +0800

    initial state of todo app form add func delete and done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,10 @@
+import Todo from "./pages/Todo";
+
 export default function App() {
-  return <h1>Todo App</h1>;
+  return (
+    <>
+      <h1>Todo App</h1>
+      <Todo />
+    </>
+  );
 }
--- frontend/src/pages/Todo.jsx
+++ frontend/src/pages/Todo.jsx
@@ -1,13 +1,53 @@
+import { useState } from "react";
+
 export default function Todo() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const [done, setDone] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setInput("");
+    setTodos((prev) => [...prev, input]);
+  }
   return (
     <>
-      <form>
-        <input type="text" />
-        <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo"
+          name="todo"
+          value={input}
+          onChange={(e) => setInput((prev) => (e.target.name = e.target.value))}
+        />
+        <button type="submit">Add</button>
       </form>
-      <ul>
-        <li>Test</li>
-      </ul>
+
+      {todos.length === 0 ? (
+        <h3>No todo's</h3>
+      ) : (
+        <>
+          <h3>Todo's</h3>
+          <ul>
+            {todos.map((item, el) => (
+              <li
+                key={el}
+                style={{ textDecoration: done ? "line-through" : "" }}
+                onClick={() => setDone(!done)}
+              >
+                {item}
+                <button
+                  onClick={() =>
+                    setTodos((prev) => prev.filter((item, i) => i !== el))
+                  }
+                >
+                  &times;
+                </button>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
